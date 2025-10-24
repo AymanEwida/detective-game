ed334fe81b6e468d13fffcdd7bac531b22406d41
AymanEwida <ayman25a.05@gmail.com>
2025-10-24 13:07:37 +0300

    chore: formatting

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,13 @@ use detective_game::game::player::{PlayerInteraction, PlayerMouseInteraction};
 use detective_game::game::store::StoreItem;
 use detective_game::renderer::button::ButtonAction;
 use detective_game::renderer::render::MouseInteraction;
-use glfw::{fail_on_errors, flush_messages, Action, Context, Key, OpenGlProfileHint, WindowEvent, WindowHint, WindowMode};
-
-use detective_game::library::constants::{
-    FPS,
-    HEIGHT,
-    WIDTH
+use glfw::{
+    fail_on_errors, flush_messages, Action, Context, Key, OpenGlProfileHint, WindowEvent,
+    WindowHint, WindowMode,
 };
+
 use detective_game::game::{character::Direction, level::GameLevel, player::Player};
+use detective_game::library::constants::{FPS, HEIGHT, WIDTH};
 use detective_game::renderer::{render::Render, styles::Size, vertice::Position};
 
 fn main() {
@@ -27,7 +26,9 @@ fn main() {
     glfw.window_hint(WindowHint::OpenGlForwardCompat(true));
     glfw.window_hint(WindowHint::Resizable(true));
 
-    let (mut window, events) = glfw.create_window(WIDTH, HEIGHT, "Derective Game", WindowMode::Windowed).expect("Failed on window creation.");
+    let (mut window, events) = glfw
+        .create_window(WIDTH, HEIGHT, "Derective Game", WindowMode::Windowed)
+        .expect("Failed on window creation.");
 
     window.make_current();
     window.set_key_polling(true);
@@ -37,187 +38,208 @@ fn main() {
 
     let (window_width, window_height) = window.get_framebuffer_size();
 
-    let mut render = Render::new(Size{ width: window_width as f32, height: window_height as f32 }).expect("Failed to created a render.");
+    let mut render = Render::new(Size {
+        width: window_width as f32,
+        height: window_height as f32,
+    })
+    .expect("Failed to created a render.");
 
     let mut player = Player::new(Position { x: 90.0, y: 180.0 }, true);
     let mut level = GameLevel::default();
     level.set_level(1);
-    level.load_level(&mut player).expect("Unable to load level!");
+    level
+        .load_level(&mut player)
+        .expect("Unable to load level!");
 
-    let mut store_items = vec![
-        StoreItem {
-            image_path: "assets/game/bullet.png",
-            title: String::from("Test"),
-            description: String::from("test 123 helloo!!"),
-            price: 2,
-            error_message: String::new(),
-            buy_func: Box::new(|player: &mut Player<'_>| {
-                print!("player size is: {:?}\n", player.get_size());
-                Ok(())
-            })
-        }
-    ];
-    
+    let mut store_items = vec![StoreItem {
+        image_path: "assets/game/bullet.png",
+        title: String::from("Test"),
+        description: String::from("test 123 helloo!!"),
+        price: 2,
+        error_message: String::new(),
+        buy_func: Box::new(|player: &mut Player<'_>| {
+            print!("player size is: {:?}\n", player.get_size());
+            Ok(())
+        }),
+    }];
+
     let mut last_update = Instant::now();
 
-    window.set_framebuffer_size_callback(| window, new_width, new_height | {
+    window.set_framebuffer_size_callback(|window, new_width, new_height| {
         window.set_size(new_width, new_height);
     });
 
     let mut cursor_position = Position { x: 0.0, y: 0.0 };
 
-    let mut pressed_buttons = HashSet::new(); 
-    
+    let mut pressed_buttons = HashSet::new();
+
     while !window.should_close() {
-        
         let (fb_window_width, fb_window_height) = window.get_framebuffer_size();
-        let (window_width, window_height) = window.get_size(); 
+        let (window_width, window_height) = window.get_size();
 
         let render_size = render.get_size();
 
-        if (fb_window_width as f32 != render_size.width) || (fb_window_height as f32 != render_size.height) {
-            render.resize(Size { width: window_width as f32, height: window_height as f32});
+        if (fb_window_width as f32 != render_size.width)
+            || (fb_window_height as f32 != render_size.height)
+        {
+            render.resize(Size {
+                width: window_width as f32,
+                height: window_height as f32,
+            });
         }
 
         glfw.poll_events();
-        
+
         for (_, event) in flush_messages(&events) {
             match event {
                 WindowEvent::CursorPos(x, y) => {
                     cursor_position = Position {
                         x: x as f32 * (fb_window_width as f32 / window_width as f32),
-                        y: y as f32 * (fb_window_height as f32 / window_height as f32)
+                        y: y as f32 * (fb_window_height as f32 / window_height as f32),
                     };
-                },
+                }
 
                 WindowEvent::MouseButton(mouse_button, action, _) => {
-                    player.set_mouse_interaction(Some(PlayerMouseInteraction::new(mouse_button, action, cursor_position)));
-                    render.set_mouse_interaction(Some(MouseInteraction::new(cursor_position, mouse_button, action)));
+                    player.set_mouse_interaction(Some(PlayerMouseInteraction::new(
+                        mouse_button,
+                        action,
+                        cursor_position,
+                    )));
+                    render.set_mouse_interaction(Some(MouseInteraction::new(
+                        cursor_position,
+                        mouse_button,
+                        action,
+                    )));
 
                     match action {
                         Action::Press => {
                             pressed_buttons.insert(mouse_button);
-                        },
+                        }
 
                         Action::Release => {
                             pressed_buttons.remove(&mouse_button);
-                        },
+                        }
 
                         _ => (),
                     }
-                },
+                }
 
                 WindowEvent::Key(key, _, action, _) => {
                     player.set_interaction(Some(PlayerInteraction::new(key, action)));
 
                     match key {
-                        Key::Escape => {
-                            match action {
-                                Action::Release => {
-                                    level.set_is_paused(!level.get_is_paused());
-                                },
-                                _ => ()
+                        Key::Escape => match action {
+                            Action::Release => {
+                                level.set_is_paused(!level.get_is_paused());
                             }
+                            _ => (),
                         },
 
-                        Key::W => {
-                            match action {
-                                Action::Press | Action::Repeat => {
-                                    if !player.get_is_using_ability() {
-                                        player.move_player(Direction::Up);
-                                    }
-                                },
-                                _ => ()
+                        Key::W => match action {
+                            Action::Press | Action::Repeat => {
+                                if !player.get_is_using_ability() {
+                                    player.move_player(Direction::Up);
+                                }
                             }
+                            _ => (),
                         },
 
-                        Key::S => {
-                            match action {
-                                Action::Press | Action::Repeat => {
-                                    if !player.get_is_using_ability() {
-                                        player.move_player(Direction::Down);
-                                    }
-                                },
-                                _ => ()
+                        Key::S => match action {
+                            Action::Press | Action::Repeat => {
+                                if !player.get_is_using_ability() {
+                                    player.move_player(Direction::Down);
+                                }
                             }
+                            _ => (),
                         },
 
-                        Key::A => {
-                            match action {
-                                Action::Press | Action::Repeat => {
-                                    if !player.get_is_using_ability() {
-                                        player.move_player(Direction::Left);
-                                    }
-                                },
-                                _ => ()
+                        Key::A => match action {
+                            Action::Press | Action::Repeat => {
+                                if !player.get_is_using_ability() {
+                                    player.move_player(Direction::Left);
+                                }
                             }
+                            _ => (),
                         },
 
-                        Key::D => {
-                            match action {
-                                Action::Press | Action::Repeat => {
-                                    if !player.get_is_using_ability() {
-                                        player.move_player(Direction::Right);
-                                    }
-                                },
-                                _ => ()
+                        Key::D => match action {
+                            Action::Press | Action::Repeat => {
+                                if !player.get_is_using_ability() {
+                                    player.move_player(Direction::Right);
+                                }
                             }
+                            _ => (),
                         },
 
-                        Key::Q => {
-                            match action {
-                                Action::Repeat => {
-                                    if level.get_status() == &LevelStatus::NotDetermine {
-                                        player.set_is_using_ability(true);
-                                    }
-                                },
-
-                                Action::Release => {
-                                    player.set_is_using_ability(false);
-                                },
-
-                                _ => ()
+                        Key::Q => match action {
+                            Action::Repeat => {
+                                if level.get_status() == &LevelStatus::NotDetermine {
+                                    player.set_is_using_ability(true);
+                                }
                             }
-                        }
-                        
-                        _ => ()
+
+                            Action::Release => {
+                                player.set_is_using_ability(false);
+                            }
+
+                            _ => (),
+                        },
+
+                        _ => (),
                     }
-                },
-        
+                }
+
                 WindowEvent::Close => {
                     window.set_should_close(true);
-                },
-                
-                _ => ()
+                }
+
+                _ => (),
             }
         }
 
         for pressed_button in pressed_buttons.iter() {
-            player.set_mouse_interaction(Some(PlayerMouseInteraction::new(*pressed_button, Action::Press, cursor_position)));
-            render.set_mouse_interaction(Some(MouseInteraction::new(cursor_position, *pressed_button, Action::Press)));
+            player.set_mouse_interaction(Some(PlayerMouseInteraction::new(
+                *pressed_button,
+                Action::Press,
+                cursor_position,
+            )));
+            render.set_mouse_interaction(Some(MouseInteraction::new(
+                cursor_position,
+                *pressed_button,
+                Action::Press,
+            )));
         }
 
         let now = Instant::now();
         let delta = now.duration_since(last_update);
 
-        if delta >= Duration::from_secs_f32(1.0/FPS) {
+        if delta >= Duration::from_secs_f32(1.0 / FPS) {
             last_update = now;
 
-            if player.is_off_window(render.get_size()) || player.is_off_border(Some(level.get_boder_start_position()), level.get_boder_size()) {
+            if player.is_off_window(render.get_size())
+                || player.is_off_border(
+                    Some(level.get_boder_start_position()),
+                    level.get_boder_size(),
+                )
+            {
                 player.move_to_prev_position();
             }
 
-            level.draw(&mut player, &mut store_items, &mut render).expect("Unable to draw level");
-            
-            render.handle_buttons_events(cursor_position).expect("Unable to handle all buttons");
+            level
+                .draw(&mut player, &mut store_items, &mut render)
+                .expect("Unable to draw level");
+
+            render
+                .handle_buttons_events(cursor_position)
+                .expect("Unable to handle all buttons");
             match render.get_button_click_action() {
                 ButtonAction::RetryLevel => {
                     level.load_level(&mut player).expect("Unable to load level");
-                },
+                }
 
                 ButtonAction::Exit => {
                     window.set_should_close(true);
-                },
+                }
 
                 ButtonAction::Unpause => {
                     level.set_is_paused(false);
@@ -226,17 +248,17 @@ fn main() {
                 ButtonAction::NextLevel => {
                     level.next_level();
                     level.load_level(&mut player).expect("Unable to load level");
-                },
+                }
 
                 ButtonAction::BuyStoreItem(idx) => {
                     store_items[idx].buy(&mut player);
                 }
 
-                ButtonAction::None => ()
+                ButtonAction::None => (),
             }
 
             render.render().expect("Uable to render object on window");
-            
+
             window.swap_buffers();
 
             player.set_interaction(None);
